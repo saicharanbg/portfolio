9c9b632c12cc36866960db9bd5f0d399c2b6c72d
saicharanbg <124761950+saicharanbg@users.noreply.github.com>
2024-08-06 00:42:30 +0530

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -33,40 +33,5 @@ export default [
          '/assets/3/5.jpg',
          '/assets/3/6.jpg',
       ]
-   },
-   {
-      title: 'Extension Adapter',
-      imgUrls: [
-         '/assets/1/1.jpg',
-         '/assets/1/2.jpg',
-         '/assets/1/3.jpg',
-         '/assets/1/4.jpg',
-         '/assets/1/5.jpg',
-         '/assets/1/6.jpg',
-         '/assets/1/7.jpg',
-      ]
-   },
-   {
-      title: 'Sumo bot',
-      imgUrls: [
-         '/assets/2/1.jpg',
-         '/assets/2/2.jpg',
-         '/assets/2/3.jpg',
-         '/assets/2/4.jpg',
-         '/assets/2/5.jpg',
-         '/assets/2/6.jpg',
-         '/assets/2/7.jpg',
-      ]
-   },
-   {
-      title: 'Bicycle',
-      imgUrls: [
-         '/assets/3/1.jpg',
-         '/assets/3/2.jpg',
-         '/assets/3/3.jpg',
-         '/assets/3/4.jpg',
-         '/assets/3/5.jpg',
-         '/assets/3/6.jpg',
-      ]
-   },
+   }
 ]
